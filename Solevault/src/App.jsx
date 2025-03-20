437299e856419158127f505e1de7db6c2c871bf6
keerthan kodi <keerthan1675@gmail.com>
2025-03-20 14:49:06 +0530

    created react components and completed login and signup page

--- Solevault/src/App.jsx
+++ Solevault/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import Header from "./components/auth/Header2"; // Ensure this path is correct
+import Login from "./pages/LoginPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Header /> {/* Navbar is always visible */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
