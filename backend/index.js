e19e205a4d0412519d56fe4d94434ed5e49edb95
keerthan kodi <keerthan1675@gmail.com>
2025-03-28 10:13:35 +0530

    created post api endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
+// backend/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const sneakerRoutes = require('./routes/sneakerRoutes'); // Import routes
 
 dotenv.config();
 
-const app = express(); 
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
-//   app.get('/', (req, res) => {
-//     res.send('SoleVault Backend is running!');
-//   });
-
+// Use sneaker routes
+app.use('/sneakers', sneakerRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
