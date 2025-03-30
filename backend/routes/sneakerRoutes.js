693f91cfbec5244273aae30ede5631ea7bc88768
keerthan kodi <keerthan1675@gmail.com>
2025-03-30 12:33:36 +0530

    added get api endpoint

--- backend/routes/sneakerRoutes.js
+++ backend/routes/sneakerRoutes.js
@@ -5,7 +5,7 @@ const Sneaker = require('../models/sneakerSchema'); // Adjust path
 const router = express.Router();
 
 // Create a new sneaker
-router.post('/sneaker', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const sneaker = new Sneaker(req.body);
     await sneaker.save();
@@ -15,28 +15,28 @@ router.post('/sneaker', async (req, res) => {
   }
 });
 
-// // Get all sneakers
-// router.get('/', async (req, res) => {
-//   try {
-//     const sneakers = await Sneaker.find();
-//     res.json(sneakers);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+// Get all sneakers
+router.get('/', async (req, res) => {
+  try {
+    const sneakers = await Sneaker.find();
+    res.json(sneakers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // // Get a sneaker by ID
-// router.get('/:id', async (req, res) => {
-//   try {
-//     const sneaker = await Sneaker.findById(req.params.id);
-//     if (!sneaker) {
-//       return res.status(404).json({ message: 'Sneaker not found' });
-//     }
-//     res.json(sneaker);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+router.get('/:id', async (req, res) => {
+  try {
+    const sneaker = await Sneaker.findById(req.params.id);
+    if (!sneaker) {
+      return res.status(404).json({ message: 'Sneaker not found' });
+    }
+    res.json(sneaker);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // // Update a sneaker by ID
 // router.put('/:id', async (req, res) => {
